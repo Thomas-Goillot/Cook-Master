a6bb3d48af1a7ceec563b8dc2d8727260ef7896b
theo <theo.berquer05@gmail.com>
2023-04-24 15:13:23 +0200

    feat  -Product page: All conditions,Product edit (per id not ok, add js)
    add js to toggle display of a div [almost finished]

--- Web/Views/shop/form.php
+++ Web/Views/shop/form.php
@@ -13,15 +13,19 @@
 
                    <div class="form-group d-flex flex-column align-items-center">
                        <label class="space">Disponibilité à la vente</label>
-                       <input type="checkbox" data-toggle="switchery" name="disponibilitySale" data-color="#9e1b21" />
+                       <input type="checkbox" data-toggle="switchery" name="disponibilitySale" id="price_display" data-color="#9e1b21" />
+                   <div id="price">
                        <label class="space">Prix de la vente</label>
-                       <input type="text" data-toggle="touchspin" name="price_purchase" data-step="1" value="0" data-bts-postfix="€" class="form-control" data-color="#df3554" />
+                       <input type="text" data-toggle="touchspin" name="price_purchase" data-step="1" value="0" data-bts-postfix="€" class="form-control" data-color="#df3554" id="price_default"/>
+                </div>
                    </div>
                    <div class="form-group d-flex flex-column align-items-center">
                        <label class="space">Disponibilité à la location</label>
-                       <input type="checkbox" data-toggle="switchery" name="disponibilityRental" data-color="#9e1b21" />
+                       <input type="checkbox" data-toggle="switchery" name="disponibilityRental" id="rent_display" data-color="#9e1b21" />
+                       <div id="rent">
                        <label class="space">Prix de la location</label>
-                       <input type="text" data-toggle="touchspin" name="price_rental" data-step="1" value="0" data-bts-postfix="€" class="form-control" data-color="#df3554" />
+                       <input type="text" data-toggle="touchspin" name="price_rental" data-step="1" value="0" data-bts-postfix="€" class="form-control" data-color="#df3554" id="rent_default"/>
+                       </div>
                    </div>
 
                    <div class="form-group d-flex flex-column align-items-center">
@@ -30,9 +34,52 @@
                    </div>
                    <div class="form-group">
                        <label class="space">Nombre de stockage disponible</label>
-                       <input type="number" name="disponibilityStock" min="0" class="form-control">
+                       <input type="number" name="disponibilityStock" min="0" required="" class="form-control">
                    </div>
                    </div>
                    <div class="d-flex justify-content-center align-items-center plusgros">
                        <button type="submit" class="btn btn-primary btn-block w-25">Ajouter</button>
                    </div>
+
+
+
+
+<!-- <script type="text/javascript">
+
+
+const checkbox = document.getElementById('price_display');
+const priceDiv = document.getElementById('price');
+const priceDefault = document.getElementById('price_default');
+
+const checkbox_rent = document.getElementById('rent_display');
+const rentDiv = document.getElementById('rent');
+const rentDefault = document.getElementById('rent_default');
+
+priceDiv.style.display = 'none';
+
+rentDiv.style.display = 'none';
+
+
+checkbox.addEventListener('click', function() {
+
+  if (checkbox.checked) {
+    priceDiv.style.display = 'block';
+  } else {
+    priceDiv.style.display = 'none';
+    priceDefault.value = "0";
+  }
+});
+
+
+checkbox_rent.addEventListener('click', function() {
+
+  if (checkbox_rent.checked) {
+    rentDiv.style.display = 'block';
+  } else {
+    rentDiv.style.display = 'none';
+    rentDefault.value = "0";
+    
+  }
+});
+
+</script> -->
