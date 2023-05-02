1c47f075eb79e187313036ce25f9622278ac3005
Thomas <222teegee@gmail.com>
2023-05-02 23:28:08 +0200

    feat - lots of improvements onthe ui and the request working with GET

---
+++ Curl/dbConnexion.c
@@ -0,0 +1,125 @@
+#include <mysql.h>
+#include <stdio.h>
+
+int main()
+{
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+
+    const char *server = "sportplus.ddns.net";
+    const char *user = "cookedmaster_dev";
+    const char *password = "lk994jQZaAIDsggS";
+    const char *database = "cookedmaster_dev";
+
+    conn = mysql_init(NULL);
+
+    if (!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        return 1;
+    }
+
+    if (mysql_query(conn, "SELECT * FROM users"))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        return 1;
+    }
+
+    res = mysql_use_result(conn);
+
+    while ((row = mysql_fetch_row(res)) != NULL)
+    {
+        //count number of columns
+        unsigned int num_fields = mysql_num_fields(res);
+
+        //print column name
+        for (int i = 0; i < num_fields; i++)
+        {
+            printf("%s ", mysql_fetch_field_direct(res, i)->name);
+        }
+        for(int i = 0; i < num_fields; i++)
+        {
+            printf("%s \n", row[i] ? row[i] : "NULL");
+        }
+    }
+
+    mysql_free_result(res);
+    mysql_close(conn);
+
+    return 0;
+}
+
+/*
+
+    //create a new request
+    CURL *curl = curl_easy_init();
+    CURLcode res;
+    char responseStr[4096] = { 0 };
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+
+        // On définit la fonction de rappel qui sera appelée avec les données de la réponse
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+
+        // On définit où les données de la réponse seront écrites
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, responseStr);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            fprintf(stderr, "Erreur lors de la requête CURL: %s\n", curl_easy_strerror(res));
+        }
+        else {
+
+            // Si la requête a réussi, on analyse la réponse JSON
+            json_error_t error;
+            json_t *root = json_loads(responseStr, 0, &error);
+            if (root) {
+
+                // Si l'analyse a réussi, on extrait les clés demandées
+                for (size_t i = 0; i < keysSize; i++) {
+                    json_t *value = getValueFromJson(root, keys[i]);
+
+                    if (value) {
+                        // On affiche la valeur de la clé
+                        const char *valueStr = json_string_value(value);
+                        if (valueStr) {
+                            gtk_text_buffer_insert_at_cursor(buffer, valueStr, -1);
+                        }
+                        else {
+                            gtk_text_buffer_insert_at_cursor(buffer, "null", -1);
+                        }
+                    }
+                    else {
+                        gtk_text_buffer_insert_at_cursor(buffer, "null", -1);
+                    }
+
+                    gtk_text_buffer_insert_at_cursor(buffer, "\n", -1);
+                }
+
+                // On libère la mémoire allouée pour l'analyse de la réponse JSON
+                json_decref(root);
+            }
+            else {
+                fprintf(stderr, "Erreur lors de l'analyse JSON: %s\n", error.text);
+            }
+        }
+
+        // On libère la mémoire allouée pour la requête CURL
+        curl_easy_cleanup(curl);
+    }
+    else {
+        fprintf(stderr, "Erreur lors de l'initialisation de CURL\n");
+    }
+
+    gtk_widget_show_all(window);
+
+    //free the memory
+    g_free(api_name);
+    g_free(method);
+    g_free(url);
+    g_free(api_key);
+
+ */
--- Curl/main.c
+++ Curl/main.c
@@ -1,14 +1,217 @@
 #include <gtk/gtk.h>
+#include <curl/curl.h>
+#include <jansson.h>
+#include <string.h>
 
+void set_margin(GtkWidget *widget, int value)
+{
+    gtk_widget_set_margin_top(widget, value);
+    gtk_widget_set_margin_bottom(widget, value);
+    gtk_widget_set_margin_start(widget, value);
+    gtk_widget_set_margin_end(widget, value);
+}
 
+void handle_request(GtkButton *button, gpointer content);
 
 void on_button1_clicked(GtkButton *button, gpointer content)
 {
-    //create an entry widget with a label and add it to the content
-    GtkWidget *entry = gtk_entry_new();
-    gtk_entry_set_placeholder_text(GTK_ENTRY(entry), "Enter your name");
-    gtk_container_add(GTK_CONTAINER(content), entry);
+    GtkWidget *api_name = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(api_name), "Enter Api name");
+    gtk_widget_set_name(api_name, "api_name");
+    set_margin(api_name, 5);
+
+    GtkWidget *select_box = gtk_combo_box_text_new();
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(select_box), "GET");
+    gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(select_box), "POST");
+    gtk_widget_set_name(select_box, "select_box");
+    set_margin(select_box, 5);
+
+    //create a horizontal box to hold the widgets
+    GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+    gtk_box_pack_start(GTK_BOX(hbox), api_name, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(hbox), select_box, TRUE, TRUE, 0);
+    gtk_container_add(GTK_CONTAINER(content), hbox);
+
+    GtkWidget *url = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(url), "Enter url API");
+    set_margin(url, 5);
+    gtk_widget_set_name(url, "url");
+    gtk_container_add(GTK_CONTAINER(content), url);
+
+    GtkWidget *api_key = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(api_key), "Enter Api key (optional if specified in url)");
+    set_margin(api_key, 5);
+    gtk_widget_set_name(api_key, "api_key");
+    gtk_container_add(GTK_CONTAINER(content), api_key);
+
+    //add a button on the right 
+    GtkWidget *send_request_button = gtk_button_new_with_label("Envoyer la requête");
+    gtk_widget_set_halign(send_request_button, GTK_ALIGN_END);
+    gtk_widget_set_valign(send_request_button, GTK_ALIGN_END);
+    set_margin(send_request_button, 5);
+    gtk_container_add(GTK_CONTAINER(content), send_request_button);
+
+    g_signal_connect(send_request_button, "clicked", G_CALLBACK(handle_request), content);
+
     gtk_widget_show_all(content);
+}
+
+size_t writeCallback(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+
+    // Calculer la taille réelle des données
+    size_t realSize = size * nmemb;
+
+    char *responseStr = (char *)userdata;
+
+    // Ajouter les nouvelles données à la fin de la chaîne
+    strcat(responseStr, ptr);
+
+    return realSize;
+}
+
+json_t *getValueFromJson(json_t *root, const char *key)
+{
+    if (!json_is_object(root))
+    {
+        fprintf(stderr, "Le JSON fourni n'est pas un objet.\n");
+        return NULL;
+    }
+
+    json_t *value = NULL;
+    const char *objKey;
+
+    json_object_foreach(root, objKey, value)
+    {
+        if (strcmp(key, objKey) == 0)
+        {
+            return value;
+        }
+        else if (json_is_object(value))
+        {
+            json_t *innerValue = getValueFromJson(value, key);
+            if (innerValue != NULL)
+            {
+                return innerValue;
+            }
+        }
+        else if (json_is_array(value))
+        {
+            size_t arrSize = json_array_size(value);
+            for (size_t i = 0; i < arrSize; i++)
+            {
+                json_t *innerValue = getValueFromJson(json_array_get(value, i), key);
+                if (innerValue != NULL)
+                {
+                    return innerValue;
+                }
+            }
+        }
+    }
+
+    return NULL;
+}
+
+void handle_request(GtkButton *button, gpointer content)
+{
+    const gchar *api_name_entry, *method_entry, *url_entry, *api_key_entry;
+
+    GtkWidget *api_name_parent = gtk_container_get_children(GTK_CONTAINER(content))->next->data;
+    GtkWidget *api_name = gtk_container_get_children(GTK_CONTAINER(api_name_parent))->data;
+    GtkWidget *select_box_parent = gtk_container_get_children(GTK_CONTAINER(content))->next->data;
+    GtkWidget *select_box = gtk_container_get_children(GTK_CONTAINER(select_box_parent))->next->data;
+    GtkWidget *url = gtk_container_get_children(GTK_CONTAINER(content))->next->next->data;
+    GtkWidget *api_key = gtk_container_get_children(GTK_CONTAINER(content))->next->next->next->data;
+
+    api_name_entry = gtk_entry_get_text(GTK_ENTRY(api_name));
+    method_entry = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(select_box));
+    url_entry = gtk_entry_get_text(GTK_ENTRY(url));
+    api_key_entry = gtk_entry_get_text(GTK_ENTRY(api_key));
+
+    CURL *curl;
+    CURLcode res;
+    json_t *root;
+    json_error_t error;
+    char responseStr[4096] = {0};
+
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, url_entry);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, responseStr);
+
+        //change the method
+        if (strcmp(method_entry, "GET") == 0)
+        {
+            curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
+        }
+        else if (strcmp(method_entry, "POST") == 0)
+        {
+            curl_easy_setopt(curl, CURLOPT_POST, 1L);
+        }
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        else
+        {
+
+            root = json_loads(responseStr, 0, &error);
+            if (!root)
+            {
+                fprintf(stderr, "error: on line %d: %s\n", error.line, error.text);
+                return;
+            }
+
+            //ajouter la reponse dans le treeview
+            GtkWidget *response_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+            gtk_window_set_title(GTK_WINDOW(response_window), "Résultat de la requête");
+            GError *error = NULL;
+            GdkPixbuf *icon = gdk_pixbuf_new_from_file("assets/images/logo.png", &error);
+            gtk_window_set_default_size(GTK_WINDOW(response_window), 800, 600);
+            gtk_window_set_position(GTK_WINDOW(response_window), GTK_WIN_POS_CENTER);
+            gtk_container_set_border_width(GTK_CONTAINER(response_window), 10);
+
+            GtkWidget *response_grid = gtk_grid_new();
+            gtk_grid_set_row_spacing(GTK_GRID(response_grid), 5);
+            gtk_grid_set_column_spacing(GTK_GRID(response_grid), 5);
+            gtk_container_add(GTK_CONTAINER(response_window), response_grid);
+
+            GtkWidget *response_scroll = gtk_scrolled_window_new(NULL, NULL);
+            gtk_widget_set_hexpand(response_scroll, TRUE);
+            gtk_widget_set_vexpand(response_scroll, TRUE);
+            gtk_grid_attach(GTK_GRID(response_grid), response_scroll, 0, 0, 1, 1);
+
+            GtkWidget *response_text = gtk_text_view_new();
+            gtk_text_view_set_editable(GTK_TEXT_VIEW(response_text), FALSE);
+            gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(response_text), FALSE);
+            gtk_container_add(GTK_CONTAINER(response_scroll), response_text);
+
+            GtkTextBuffer *response_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(response_text));
+
+            GtkTextIter start, end;
+            gtk_text_buffer_get_start_iter(response_buffer, &start);
+            gtk_text_buffer_get_end_iter(response_buffer, &end);
+
+            gtk_text_buffer_insert(response_buffer, &end, responseStr, -1);
+
+            gtk_widget_show_all(response_window);
+
+
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+
+
+
+
+
+
 }
 
 void on_button2_clicked(GtkButton *button, gpointer user_data)
@@ -55,9 +258,9 @@ void generate_sidebar(GtkWidget *grid, GtkWidget *content, GtkWidget *sidebar, G
 
     /* Create three buttons in the sidebar */
     GtkWidget *button1 = gtk_button_new_with_label("Faire une requête");
-    GtkWidget *button2 = gtk_button_new_with_label("Paramètres");
-    GtkWidget *button3 = gtk_button_new_with_label("Listes des APIs");
-    GtkWidget *button4 = gtk_button_new_with_label("Historique des requêtes");
+    GtkWidget *button2 = gtk_button_new_with_label("Listes des APIs");
+    GtkWidget *button3 = gtk_button_new_with_label("Historique des requêtes");
+    GtkWidget *button4 = gtk_button_new_with_label("Paramètres");
     GtkWidget *button5 = gtk_button_new_with_label("Quitter");
     gtk_container_add(GTK_CONTAINER(sidebar), button1);
     gtk_container_add(GTK_CONTAINER(sidebar), button2);
@@ -115,11 +318,6 @@ int main(int argc, char *argv[])
 
     GtkWidget *label = gtk_label_new("Bienvenue sur l'application Cook Master Request Api");
     gtk_container_add(GTK_CONTAINER(content), label);
-
-
-
-
-
 
     /* Create a new sidebar */
     generate_sidebar(grid, content, sidebar, title);
