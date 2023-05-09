64db5b41718cc2464d878d325ef0257a216f843c
Thomas <222teegee@gmail.com>
2023-05-09 23:33:03 +0200

    refactor - automate config file include

--- Web/config/config.php
+++ Web/config/config.php
@@ -27,30 +27,13 @@ define('PASSWORD_COST', 12);
 define('LOGO_SVG', 'assets/images/logo.svg');
 define('LOGO_PNG', 'assets/images/logo.png');
 
-//CONST FOR LENGHT OF columns in table users
-include_once("constant/users.php");
 
-//CONST FOR TYPE OF ACCESS
-include_once("constant/access.php");
+// INCLUDE ALL THE CONSTANTS FILES IN THE CONFIG/CONSTANT FOLDER
+$directory = 'config/constant';
+foreach (scandir($directory) as $file) {
+    $path = $directory . '/' . $file;
+    if (is_file($path)) include_once($path);
+}
 
-//CONST CONST FOR LENGHT OF columns in table subscriptions
-include_once("constant/subscription.php");
-
-//ALERT MESSAGES
-include_once("constant/alert.php");
-
-//CONST FOR LENGHT OF columns in table event_template
-include_once("constant/eventTemplate.php");
-
-//CONST FOR LENGHT OF columns in table event
-include_once("constant/events.php");
-
-//CONST FOR LOCATION FONCTIONNALITIES
-include_once("constant/location.php");
-
-//const for equipment
-include_once("constant/equipment.php");
-
-include_once("constant/apiKey.php");
 
 ?>
