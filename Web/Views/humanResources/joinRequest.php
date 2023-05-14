a9754cc6b6895220a6b8566e351b334f20d7921f
Jiullian <jiullian77470@gmail.com>
2023-05-14 21:57:54 +0200

    fix - presentation of request

--- Web/Views/humanResources/joinRequest.php
+++ Web/Views/humanResources/joinRequest.php
@@ -3,36 +3,19 @@
 ?>
 
 <div class="row">
-    <div class="col-12">
-         <div class="card">
-            <div class="card-body">
-                <h4 class="card-title">Candidatures postées par les utilisateurs</h4>
-                <table id="basic-datatable" class="table dt-responsive nowrap">
-                    <thead>
-                        <tr>
-                            <th>Photo</th>
-                            <th>Identité</th>
-                            <th>Téléphone</th>
-                            <th>Email</th>
-                            <th>Numéro de siret</th>
-                            <th>CV</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                            foreach($getAllRequest as $request){
-                                echo '<td><img src="' . $path_prefix  . 'assets/images/request/pictures/' . $request['image'] . '" class="img-thumbnail"></td>';
-                                echo '<td>' . $request['name'] . ' ' . $request['surname'] . '</td>';
-                                echo '<td>' . $request['phone'] . '</td>';
-                                echo '<td>' . $request['email'] . '</td>';
-                                echo '<td>' . $request['siret'] . '</td>';
-                                echo '<td><embed src="' . $path_prefix  . 'assets/images/request/cv/' . $request['file'] . '" class="img-thumbnail" type="application/pdf"></td>';
-                            };
-                        ?>
-                    </tbody>
-                </table>
-            </div> <!-- end card body-->
-        </div> <!-- end card -->
-    </div><!-- end col-->
+    <?php foreach($getAllRequest as $request): ?>
+        <div class="col-lg-3 mb-3">
+            <div class="card">
+                <img src="<?php echo $path_prefix . 'assets/images/request/pictures/' . $request['image']; ?>" class="card-img-top" alt="Photo">
+                <div class="card-body text-center">
+                    <h5 class="card-title"><?php echo $request['name'] . ' ' . $request['surname']; ?></h5>
+                    <p class="card-text"><strong>Téléphone :</strong> <?php echo $request['phone']; ?></p>
+                    <p class="card-text"><strong>Email :</strong> <?php echo $request['email']; ?></p>
+                    <p class="card-text"><strong>Numéro de siret :</strong> <?php echo $request['siret']; ?></p>
+                    <p class="card-text"><strong>Métier recherché :</strong> <?php echo $request['type']; ?></p>
+                    <a href="<?php echo $path_prefix . 'assets/images/request/cv/' . $request['file']; ?>" target="_blank" class="btn btn-primary">Voir CV</a>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
 </div>
-<!-- end row-->
