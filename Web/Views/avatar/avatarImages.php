492f31367e8f1cea7981ca4e16faa87ed2c202b4
Thomas <222teegee@gmail.com>
2023-05-30 19:57:51 +0200

    fix - DO NOT CHECKOUT

--- Web/Views/avatar/avatarImages.php
+++
@@ -1,5 +0,0 @@
-<img src="../assets/images/avatar/head/<?= $avatar['head'] ?> " class="img-fluid rounded-pill" id="head" alt="" loading="lazy">
-<img src="../assets/images/avatar/eyes/<?= $avatar['eyes'] ?> " class="img-fluid" id="eyes" alt="" loading="lazy">
-<img src="../assets/images/avatar/nose/<?= $avatar['nose'] ?> " class="img-fluid" id="nose" alt="" loading="lazy">
-<img src="../assets/images/avatar/mouth/<?= $avatar['mouth'] ?> " class="img-fluid" id="mouth" alt="" loading="lazy">
-<img src="../assets/images/avatar/brows/<?= $avatar['brows'] ?> " class="img-fluid" id="brows" alt="" loading="lazy">
