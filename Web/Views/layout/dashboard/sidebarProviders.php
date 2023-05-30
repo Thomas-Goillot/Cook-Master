c65bc84e2d7dc827e3541c8f3cdf51726173795b
Thomas <222teegee@gmail.com>
2023-05-30 20:07:27 +0200

    feat - change name in sidebar

--- Web/Views/layout/dashboard/sidebarProviders.php
+++ Web/Views/layout/dashboard/sidebarProviders.php
@@ -2,6 +2,6 @@
 
 <li><a href="<?= $path_prefix ?>registrationService" class="waves-effect"><i class="fas fa-file-invoice"></i><span>Prestations</span></a></li>
 
-<li><a href="<?= $path_prefix ?>courseService" class="waves-effect"><i class="bx bx-list-ol"></i><span>Cours disponible</span></a></li>
+<li><a href="<?= $path_prefix ?>courseService" class="waves-effect"><i class="bx bx-list-ol"></i><span>Demande Cours</span></a></li>
 
 <li><a href="<?= $path_prefix ?>courseService/providersCourses" class="waves-effect"><i class="bx bxs-book-open"></i><span>Vos Cours</span></a></li>
