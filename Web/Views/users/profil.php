b91ac895e87c7ed34237eea9c960b45215297c25
Thomas <222teegee@gmail.com>
2023-06-08 16:02:17 +0200

    feat - tryout ads

--- Web/Views/users/profil.php
+++ Web/Views/users/profil.php
@@ -6,5 +6,10 @@ include_once('views/layout/dashboard/path.php');
     <?php include_once('views/users/userInfo.php'); ?>
 
     <?php include_once('views/users/userCommand.php'); ?>
-<!-- <?php include_once('views/users/userEvent.php'); ?> -->
+
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7119326631938628" crossorigin="anonymous"></script>
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7119326631938628" data-ad-slot="4902882167" data-ad-format="auto" data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </div>
