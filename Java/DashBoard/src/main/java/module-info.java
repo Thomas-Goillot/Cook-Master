8b9602f2cc469b261f2b2a915ba546d762107d7d
Thomas <222teegee@gmail.com>
2023-06-11 18:04:22 +0200

    feat - first project page working fine with inteliji

---
+++ Java/DashBoard/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module cookmaster.dashboard {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens cookmaster.dashboard to javafx.fxml;
+    exports cookmaster.dashboard;
+}
