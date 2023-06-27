2650abd5acdcde171eea8976b27eeb3433fd6c6a
theo <theo.berquer05@gmail.com>
2023-06-27 19:28:39 +0200

    feat - correct android

---
+++ android/CookMaster/app/src/main/java/com/example/cookmaster/inscription.java
@@ -0,0 +1,23 @@
+package com.example.cookmaster;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class inscription extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscription);
+
+
+            }
+
+    }
+
+
+
