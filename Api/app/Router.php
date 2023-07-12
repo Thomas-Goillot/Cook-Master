5d986833fecbd16f57d434692f33dff9126d2680
Thomas <222teegee@gmail.com>
2023-07-12 23:56:01 +0200

    fix - mistake

---
+++ Api/app/Router.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App;
+
+class Router
+{
+    private $routes = [];
+
+    public function addRoute($method, $path, $handler, $middleware = null)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'path' => $path,
+            'handler' => $handler,
+            'middleware' => $middleware
+        ];
+    }
+
+    public function handleRequest($method, $uri)
+    {
+        $matchedRoute = null;
+        $routeParams = [];
+
+        foreach ($this->routes as $route) {
+            if ($route['method'] === $method && $this->matchPath($route['path'], $uri, $routeParams)) {
+                $matchedRoute = $route;
+                break;
+            }
+        }
+
+        if ($matchedRoute) {
+            $authMiddleware = $matchedRoute['middleware'] ?? null;
+            if ($authMiddleware && !$authMiddleware()) {
+                return;
+            }
+
+            $handler = $matchedRoute['handler'];
+            $handler(...$routeParams);
+        } else {
+            JsonResponse::error('Not found', 404);
+        }
+    }
+
+    private function matchPath($routePath, $requestPath, &$routeParams)
+    {
+        $routePath = trim($routePath, '/');
+        $requestPath = trim($requestPath, '/');
+
+        $routeSegments = explode('/', $routePath);
+        $requestSegments = explode('/', $requestPath);
+
+        if (count($routeSegments) !== count($requestSegments)) {
+            return false;
+        }
+
+        $numSegments = count($routeSegments);
+
+        for ($i = 0; $i < $numSegments; $i++) {
+            $routeSegment = $routeSegments[$i];
+            $requestSegment = $requestSegments[$i];
+
+            if ($routeSegment !== $requestSegment && !preg_match('/^\{.+\}$/', $routeSegment)) {
+                return false;
+            }
+
+            if (preg_match('/^\{.+\}$/', $routeSegment)) {
+                $paramName = trim($routeSegment, '{}');
+                $routeParams[$paramName] = $requestSegment;
+            }
+        }
+
+        return true;
+    }
+
+}
+
---
+++ Api/routes/UserRoutes.php
@@ -0,0 +1,338 @@
+<?php
+
+namespace Routes;
+
+use App\JsonRequest;
+use App\JsonResponse;
+use Model\UserRepository;
+
+class UserRoutes
+{
+
+    private function hashPassword(string $password): string
+    {
+
+        for ($i = 0; $i < 12; $i++) {
+            $password = hash('sha256', $password . 'cookmaster');
+        }
+
+        return $password;
+    }
+
+    public function login(){
+
+        $requestData = JsonRequest::getRequestBody();
+
+        $email = $requestData['email'] ?? null;
+        $password = $requestData['password'] ?? null;
+
+        if (!isset($email) || empty($email) || !isset($password) || empty($password)) {
+            JsonResponse::error('L\'email et le mot de passe sont requis', 400);
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            JsonResponse::error('L\'email n\'est pas valide', 400);
+        }
+
+        if (strlen($password) < 8) {
+            JsonResponse::error('Le mot de passe doit contenir au moins '. 8 .' caractères', 400);
+        }
+
+        $password = $this->hashPassword($password);
+
+        $userRepository = new UserRepository();
+
+
+        $user = $userRepository->getUserByEmailAndPassword($email, $password);
+
+        if (!$user) {
+            JsonResponse::error('Identifiant incorect', 404);
+        }
+
+        JsonResponse::success($user['id_users']);
+        
+    }
+
+
+    public function register(){
+
+        $requestData = JsonRequest::getRequestBody();
+
+        $email = $requestData['email'] ?? null;
+        $password = $requestData['password'] ?? null;
+        $name = $requestData['name'] ?? null;
+        $surname = $requestData['surname'] ?? null;
+        $phone = $requestData['phone'] ?? null;
+
+        if (!isset($email) || empty($email) || !isset($password) || empty($password) || !isset($name) || empty($name) || !isset($surname) || empty($surname) || !isset($phone) || empty($phone)) {
+            JsonResponse::error('L\'email, le mot de passe, le nom, le prénom et le téléphone sont requis', 400);
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            JsonResponse::error('L\'email n\'est pas valide', 400);
+        }
+
+        if (strlen($password) < 8 && strlen($password) > 35) {
+            JsonResponse::error('Le mot de passe doit contenir au moins '. 8 .' caractères', 400);
+        }
+
+        if(strlen($name) > 100){
+            JsonResponse::error('Le nom ne doit pas dépasser 100 caractères', 400);
+        }
+
+        if(strlen($surname) > 100){
+            JsonResponse::error('Le prénom ne doit pas dépasser 100 caractères', 400);
+        }
+
+        if(strlen($phone) > 25 && strlen($phone) < 10){
+            JsonResponse::error('Le numéro de téléphone ne doit pas dépasser 10 caractères', 400);
+        }
+
+        //check if the email is already used
+        $userRepository = new UserRepository();
+        $user = $userRepository->getUserByEmail($email);
+
+        if ($user) {
+            JsonResponse::error('L\'email est déjà utilisé', 400);
+        }
+
+        $password = $this->hashPassword($password);
+
+        $userRepository->createUser($name, $surname, $email, $phone, $password);
+
+        JsonResponse::success("L'utilisateur a bien été créé");
+    }
+
+
+    public function getUsers()
+    {
+        $userRepository = new UserRepository();
+
+        $users = $userRepository->getAllUsers();
+
+        JsonResponse::success($users);
+    }
+ 
+    public function createUser()
+    {
+        $requestData = JsonRequest::getRequestBody();
+
+        if (!isset($requestData['name']) || empty($requestData['name'])) {
+            JsonResponse::error('Le nom de l\'utilisateur est requis', 400);
+        }
+
+       
+    }
+
+    public function getUser($id)
+    {
+        $userRepository = new UserRepository();
+
+        $user = $userRepository->getUserById($id);
+
+        if (!$user) {
+            JsonResponse::error('Utilisateur non trouvé', 404);
+        }
+
+        JsonResponse::success($user);
+    }
+
+    public function getUserCourses($id)
+    {
+        $userRepository = new UserRepository();
+
+        $user = $userRepository->getUserById($id);
+        if (!$user) {
+            JsonResponse::error('Utilisateur non trouvé', 404);
+        }
+
+        $courses = $userRepository->getUserCourses($id);
+        if (!$courses) {
+            JsonResponse::error('Vous n\'avez pas de cours à venir', 404);
+        }
+
+        JsonResponse::success($courses);
+    }
+
+    public function updateUser(){
+        
+        $requestData = JsonRequest::getRequestBody();
+
+        $id = $requestData['id'] ?? null;
+
+        $name = $requestData['name'] ?? null;
+
+        $surname = $requestData['surname'] ?? null;
+
+        $email = $requestData['email'] ?? null;
+
+        $phone = $requestData['phone'] ?? null;
+
+        if (!isset($name) || empty($name) || !isset($surname) || empty($surname) || !isset($email) || empty($email) || !isset($phone) || empty($phone)) {
+            JsonResponse::error('Le nom, le prénom, l\'email, le téléphone et le mot de passe sont requis', 400);
+        }
+
+        if(strlen($name) > 100){
+            JsonResponse::error('Le nom ne doit pas dépasser 100 caractères', 400);
+        }
+
+        if(strlen($surname) > 100){
+            JsonResponse::error('Le prénom ne doit pas dépasser 100 caractères', 400);
+        }
+
+        if(strlen($phone) > 25 && strlen($phone) < 10){
+            JsonResponse::error('Le numéro de téléphone ne doit pas dépasser 10 caractères', 400);
+        }
+
+        $userRepository = new UserRepository();
+
+        $user = $userRepository->getUserById($id);
+
+        if (!$user) {
+            JsonResponse::error('Utilisateur non trouvé', 404);
+        }
+
+        $userRepository->updateUser($id, $name, $surname, $email, $phone);
+
+        JsonResponse::success("L'utilisateur a bien été modifié");
+    }
+
+    public function getShop()
+    {
+        $userRepository = new UserRepository();
+
+        $shop = $userRepository->getShop();
+        if (!$shop) {
+            JsonResponse::error('La boutique est vide', 404);
+        }
+
+        JsonResponse::success($shop);
+    }
+
+    public function getUserEvents($id)
+    {
+        $userRepository = new UserRepository();
+
+        $user = $userRepository->getUserById($id);
+        if (!$user) {
+            JsonResponse::error('Utilisateur non trouvé', 404);
+        }
+
+        $events = $userRepository->getUserEvents($id);
+        if (!$events) {
+            JsonResponse::error('Vous n\'avez pas d\'évènements à venir', 404);
+        }
+
+        JsonResponse::success($events);
+    }
+
+    public function getPastUserEvents($id)
+    {
+        $userRepository = new UserRepository();
+
+        $user = $userRepository->getPastUserEvents($id);
+        if (!$user) {
+            JsonResponse::error('Utilisateur non trouvé', 404);
+        }
+
+        $events = $userRepository->getPastUserEvents($id);
+        if (!$events) {
+            JsonResponse::error('Vous n\'avez pas d\'évènements passés', 404);
+        }
+
+        JsonResponse::success($events);
+    }
+
+    public function getAllEvents(){
+        $userRepository = new UserRepository();
+
+        $events = $userRepository->getAllEvents();
+        if (!$events) {
+            JsonResponse::error('Il n\'y a pas d\'évènements', 404);
+        }
+
+        JsonResponse::success($events);
+    }
+
+    public function getSubscription($id)
+    {
+        $userRepository = new UserRepository();
+
+        $user = $userRepository->getUserById($id);
+        if (!$user) {
+            JsonResponse::error('Utilisateur non trouvé', 404);
+        }
+
+        $sub = $userRepository->getSubscription($id);
+
+        JsonResponse::success($sub);
+    }
+
+    public function getCartUser($id){
+        $userRepository = new UserRepository();
+
+        $user = $userRepository->getUserById($id);
+        if (!$user) {
+            JsonResponse::error('Utilisateur non trouvé', 404);
+        }
+
+        $cart = $userRepository->getCartUser($id);
+
+        JsonResponse::success($cart);
+    }
+
+    public function addProductToCart(){
+        $requestData = JsonRequest::getRequestBody();
+
+        $id = $requestData['id'] ?? null;
+
+        $id_product = $requestData['id_product'] ?? null;
+
+        $quantity = $requestData['quantity'] ?? null;
+
+        $userRepository = new UserRepository();
+
+        $user = $userRepository->getCartUser($id);
+        if (!$user) {
+            JsonResponse::error('Utilisateur non trouvé', 404);
+        }
+
+        $cart = $userRepository->addProductToCart($user, $id_product, $quantity);
+
+        JsonResponse::success("Le produit a bien été ajouté au panier");
+    }
+
+    public function getAllProductsOfCart($id){
+        $userRepository = new UserRepository();
+
+        $user = $userRepository->getUserById($id);
+        if (!$user) {
+            JsonResponse::error('Utilisateur non trouvé', 404);
+        }
+
+        $id_cart = $userRepository->getCartUser($id);
+
+        $cart = $userRepository->getAllProductsOfCart($id_cart);
+
+        JsonResponse::success($cart);
+    }
+
+    public function deleteProductOfCart(){
+        $requestData = JsonRequest::getRequestBody();
+
+        $id = $requestData['id'] ?? null;
+
+        $id_product = $requestData['id_product'] ?? null;
+
+        $userRepository = new UserRepository();
+
+        $user = $userRepository->getCartUser($id);
+        if (!$user) {
+            JsonResponse::error('Utilisateur non trouvé', 404);
+        }
+
+        $cart = $userRepository->deleteProductOfCart($user, $id_product);
+
+        JsonResponse::success("Le produit a bien été supprimé du panier");
+    }
+}
